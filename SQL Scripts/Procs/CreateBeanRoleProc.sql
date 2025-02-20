78572ff4f9af717939047353741f9d924d2d72c2
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-02-20 16:39:14 +0200

    Modified privileges

--- SQL Scripts/Procs/CreateBeanRoleProc.sql
+++ SQL Scripts/Procs/CreateBeanRoleProc.sql
@@ -6,8 +6,7 @@ CREATE PROCEDURE CreateBeanRole()
 BEGIN -- Create the role if it doesn’t exist
 CREATE ROLE IF NOT EXISTS 'bean_role';
 -- Grant EXECUTE permission on specific stored procedures
-GRANT EXECUTE ON PROCEDURE Hitman_Association.SomeProcedure TO 'bean_role';
-GRANT EXECUTE ON PROCEDURE Hitman_Association.AnotherProcedure TO 'bean_role';
+GRANT EXECUTE ON PROCEDURE Hitman_Association.PromoteBeanNextLevelProc TO 'bean_role';
 -- Apply changes
 FLUSH PRIVILEGES;
 END // 
--- SQL Scripts/Procs/CreateOrgRoleProc.sql
+++ SQL Scripts/Procs/CreateOrgRoleProc.sql
@@ -8,8 +8,7 @@ CREATE ROLE IF NOT EXISTS 'org_role';
 -- Grant permissions to insert into the `hits` table
 GRANT INSERT ON Hitman_Association.hits TO 'org_role';
 -- Grant EXECUTE permission on specific stored procedures
-GRANT EXECUTE ON PROCEDURE Hitman_Association.SomeProcedure TO 'org_role';
-GRANT EXECUTE ON PROCEDURE Hitman_Association.AnotherProcedure TO 'org_role';
+GRANT EXECUTE ON PROCEDURE Hitman_Association.CreateHitContract TO 'org_role';
 -- Apply changes
 FLUSH PRIVILEGES;
 END // 
