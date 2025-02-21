9aa73a8e81e39b0fcadf583714dd86a059ddf308
Daniel van Zyl <daniel.vanzyl@bbd.co.za>
2025-02-21 08:17:19 +0200

    Testing Procs

--- SQL Scripts/sources/Setup.sql
+++ SQL Scripts/sources/Setup.sql
@@ -1,8 +1,8 @@
-SOURCE Database_Creation_Query.sql;
-SOURCE Data_Insert_Query.sql;
-SOURCE Run_All_Triggers_Query.sql;
-SOURCE Database_Procs_Query.sql;
-SOURCE Database_Events_Query.sql;
-SOURCE Views/Hit_Details.view.sql;
-SOURCE Views/Contract_Details.view.sql;
-SOURCE Views/Bean_Details.view.sql;
+SOURCE ../migrations/sql/V1__Database_Creation_Query.sql;
+SOURCE ../migrations/sql/V2__Data_Insert_Query.sql;
+SOURCE ./Run_All_Triggers_Query.sql;
+SOURCE ./Database_Procs_Query.sql;
+SOURCE ./Database_Events_Query.sql;
+SOURCE ../migrations/sql/Views/R__Hit_Details.view.sql;
+SOURCE ../migrations/sql/Views/R__Contract_Details.view.sql;
+SOURCE ../migrations/sql/Views/R__Bean_Details.view.sql;
